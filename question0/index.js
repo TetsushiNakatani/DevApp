416526ab3321710bf41a37ca97498c75206a9bda
TN-note\tetsushi nakatani <appli@hracn.com>
2018-05-07 19:26:57 +0900

    課題１　開発時間10分　参考ページなし

--- question0/index.js
+++ question0/index.js
@@ -1,4 +1,10 @@
-console.log('hello world')
-
 const num = window.prompt('入力してください。')
-console.log(`入力値は${num}です。`)
+for (let n = 1; n <= num; n++) {
+  if (n % 3 === 0) {
+    console.log(`Fizz`)
+  } else if (n % 5 === 0) {
+    console.log(`Buzz`)
+  } else {
+    console.log(`${n}`)
+  }
+}
