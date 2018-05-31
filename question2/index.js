b8b4b91bc4002ac2250b47f87c06429589eb3cc8
TN-note\tetsushi nakatani <appli@hracn.com>
2018-05-31 21:09:39 +0900

    課題２　バグを修正　無駄な関数を除去　For分一回でハッシュ作成完了 課題３　名称修正　無駄な処理除去　０，１，２以外が入力されたときに処理を追加

--- question2/index.js
+++ question2/index.js
@@ -1,14 +1,14 @@
-// 文字列の中で同じ単語をカウントする関数
-const counter = function(str) {
-  return inputstr.split(str).length - 1
-}
 const inputstr = window.prompt('文章を入力してください。').replace('　', ' ') // 全角空白を半角の空白に置換
 const arry_str = inputstr.split(' ') // 入力文字を半角の空白で分割
-const buf_arry_str = {} // 出力用のオブジェクトの初期化
+const buf_obj_str = {} // 出力用のオブジェクトの初期化
 for (let n = 0; n < arry_str.length; n++) {
-  buf_arry_str[arry_str[n]] = counter(arry_str[n])
+  if (!buf_obj_str[arry_str[n]]) {
+    buf_obj_str[arry_str[n]] = 1
+  } else {
+    buf_obj_str[arry_str[n]] = buf_obj_str[arry_str[n]] + 1
+  }
 }
-console.log(buf_arry_str)
+console.log(buf_obj_str)
 
 // 作業時間約15分 + 前回の作業時間30分
 // 参考サイトは特になし
--- question3/index.js
+++ question3/index.js
@@ -1,29 +1,34 @@
 // ランダムで０，１，２を返す関数
-const play = function() {
+const randomFanc = function() {
   return Math.floor(Math.random() * 3)
 }
-const rock_paper_scissors = { 0: 'グー', 1: 'チョキ', 2: 'パー' }
+
+const wording = ['「アイコでしょ！」', '「あなたの勝ち！」', '「あなたの負け！」']
+const rockPaperScissors = { 0: 'グー', 1: 'チョキ', 2: 'パー' }
 const combination = [
-  ['「アイコでしょ！」', '「あなたの勝ち！」', '「あなたの負け！」'], // ユーザー：グー　[あいこ、勝ち、負け]
-  ['「あなたの負け！」', '「アイコでしょ！」', '「あなたの勝ち！」'], // ユーザー：チョキ　[負け、あいこ、勝ち]
-  ['「あなたの勝ち！」', '「あなたの負け！」', '「アイコでしょ！」'], // ユーザー：パー　[勝ち、負け、あいこ]
+  [wording[0], wording[1], wording[2]], // ユーザー：グー　[あいこ、勝ち、負け]
+  [wording[2], wording[0], wording[1]], // ユーザー：チョキ　[負け、あいこ、勝ち]
+  [wording[1], wording[2], wording[0]], // ユーザー：パー　[勝ち、負け、あいこ]
 ]
 
-let inputstr = window.prompt('「じゃんけん・・・」\n 0.グー 1.チョキ 2.パー') // 入力
-let result = true
-while (result === true) {
-  console.log('「ぽい！」')
-  const comp = play()
-  console.log('＊コンピュータ：' + rock_paper_scissors[comp])
-  console.log('＊あなた ：' + rock_paper_scissors[inputstr])
-  if (comp === parseInt(inputstr)) {
-    console.log(combination[inputstr][comp])
-    inputstr = window.prompt('「じゃんけん・・・」\n 0.グー 1.チョキ 2.パー') // 入力
-    result = true
+let loopBool = true
+while (loopBool === true) {
+  const inputstr = window.prompt('「じゃんけん・・・」\n 0.グー 1.チョキ 2.パー') // 入力
+  if (!inputstr || isNaN(inputstr) || parseInt(inputstr) > 2) {
+    alert('ただ数数値を入力してください。') // 入力
+    loopBool = true
   } else {
-    console.log(combination[inputstr][comp])
-    result = false
-    break
+    console.log('「ぽい！」')
+    const comp = randomFanc()
+    console.log('＊コンピュータ：' + rockPaperScissors[comp])
+    console.log('＊あなた ：' + rockPaperScissors[inputstr])
+    if (comp === parseInt(inputstr)) {
+      console.log(combination[inputstr][comp])
+      loopBool = true
+    } else {
+      console.log(combination[inputstr][comp])
+      loopBool = false
+    }
   }
 }
 
