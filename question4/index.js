42c584d2c447398298194e09e3a0ac3662169e78
TN-note\tetsushi nakatani <appli@hracn.com>
2018-07-17 18:51:31 +0900

    関数名を変更

--- question4/index.js
+++ question4/index.js
@@ -1,13 +1,13 @@
 const inputstr = window.prompt('好きな数字を入力してください') // 入力
-const addfunc = function(inputNum) {
+const sum = function(inputNum) {
   if (inputNum > 0) {
-    return inputNum + addfunc(inputNum - 1)
+    return inputNum + sum(inputNum - 1)
   } else {
     return 0
   }
 }
 console.log('入力：' + inputstr)
-const result = addfunc(parseInt(inputstr))
+const result = sum(parseInt(inputstr))
 console.log('出力：' + result)
 // 作業時間約15分 + 修正30分
 // 参考サイトは以下
